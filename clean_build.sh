95ecedc62c99e652bb346e78fb1b320a68107631
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-12-10 19:10:48 -0500

    adding trivial program

--- clean_build.sh
+++ clean_build.sh
@@ -1,3 +1,5 @@
+# take 1 command line argument - the name of file to be tested
+
 rm -r build
 rm -r IR_Outputs
 mkdir build
@@ -7,4 +9,4 @@ cmake ..
 make
 cd ..
 
-clang -g -o0 -fpass-plugin=build/libSeminalInputFeaturesAnalysis.so -emit-llvm -c tests/tinyobj_loader_c.h -o IR_Outputs/tinyobj_loader_c.ll
+clang -g -o0 -fpass-plugin=build/libSeminalInputFeaturesAnalysis.so -emit-llvm -c tests/{$1}.c -o IR_Outputs/{$1}.ll
